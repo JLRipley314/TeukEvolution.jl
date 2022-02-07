8b9f41bb0b0531aaeb01893992f6a557481a4299
Documenter.jl <documenter@juliadocs.github.io>
2022-02-07 11:05:45 +0000

    build based on b5245b0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TeukEvolution","category":"page"},{"location":"#TeukEvolution","page":"Home","title":"TeukEvolution","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TeukEvolution.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TeukEvolution]","category":"page"}]
+}
