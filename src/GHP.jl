eb836f769e9a008d0a13a72bebf920ab6162b308
Justin <lloydripley@gmail.com>
2022-05-13 09:36:50 -0700

    added reference

--- src/GHP.jl
+++ src/GHP.jl
@@ -1,13 +1,22 @@
 """
 Geroch-Held-Penrose (GHP) operators; see 
 
-Geroch, Held, and Penrose, 
-Journal of Mathematical Physics 14, 874 (1973)
+   Geroch, Held, and Penrose, 
+   Journal of Mathematical Physics 14, 874 (1973)
+
+For the formulas in the coordinates we use, see
+(in particular Sec V.C)
+   
+   Ripley, Loutrel, Giorgi, and Pretorius, 
+   Phys.Rev.D 103 (2021) 104018, arXiv:2010.00162,
+   
 """
 module GHP
 
 include("Fields.jl")
+include("Sphere.jl")
 using .Fields: Field
+using .Sphere: swal_raising_matrix, swal_lowering_matrix, angular_matrix_mult!
 
 export GHP_ops, set_edth!, set_edth_prime!, set_thorn!, set_thorn_prime!
 
@@ -25,6 +34,12 @@ struct GHP_ops
    pre_thorn_prime_DT::Array{ComplexF64,2}
    pre_thorn_prime_DR::Array{ComplexF64,2} 
 
+   raise::Array{ComplexF64,4}
+   lower::Array{ComplexF64,4}
+
+   smap::Dict{Int64,Int64}
+   mmap::Dict{Int64,Int64}
+
    function GHP_ops(;
          Rvals::Vector{Float64},
          Cvals::Vector{Float64},
@@ -33,10 +48,12 @@ struct GHP_ops
          bhm::Float64,
          bhs::Float64,
          cl::Float64) 
-      
+     
       nx = length(Rvals)
       ny = length(Cvals)
-   
+  
+      spins = [-2,-1,0,1,2]
+
       pre_edth_DT            = zeros(ComplexF64,nx,ny)
       pre_edth_raised        = zeros(ComplexF64,nx,ny) 
       pre_edth_level         = zeros(ComplexF64,nx,ny) 
@@ -48,7 +65,10 @@ struct GHP_ops
       pre_thorn_level        = zeros(ComplexF64,nx,ny) 
       pre_thorn_prime_DT     = zeros(ComplexF64,nx,ny) 
       pre_thorn_prime_DR     = zeros(ComplexF64,nx,ny) 
-   
+  
+      raise = zeros(ComplexF64,ny,ny,length(Mvals),length(spins))
+      lower = zeros(ComplexF64,ny,ny,length(Mvals),length(spins))
+
       for j=1:ny
          cy = Cvals[j]
          sy = Svals[j]
@@ -131,7 +151,24 @@ struct GHP_ops
             pre_thorn_prime_DR[i,j] = ((1.0/cl)^2)*R
          end
       end
-     
+    
+      for (j,s) in enumerate(spins)
+         for (i,m) in enumerate(Mvals) 
+            tmp_raise = swal_raising_matrix( ny,s,m)
+            tmp_lower = swal_lowering_matrix(ny,s,m)
+
+            raise[:,:,i,j] .= tmp_raise
+            lower[:,:,i,j] .= tmp_lower
+         end
+      end
+
+      for (i,s) in enumerate(spins)
+         smap[s]=i
+      end
+      for (i,m) in enumerate(Mvals)
+         mmap[m]=i
+      end
+
       return new(
          pre_edth_DT,
          pre_edth_raised, 
@@ -143,7 +180,11 @@ struct GHP_ops
          pre_thorn_DR, 
          pre_thorn_level, 
          pre_thorn_prime_DT, 
-         pre_thorn_prime_DR 
+         pre_thorn_prime_DR,
+         raise,
+         lower,
+         smap,
+         mmap
       )
    end
 end   
@@ -151,6 +192,7 @@ end
 function set_edth!(;
       edth ::Array{ComplexF64,2},
       spin ::Int64, 
+      m_ang::Int64, 
       boost::Int64, 
       level ::Array{ComplexF64,2}, 
       DT    ::Array{ComplexF64,2}, 
@@ -159,7 +201,9 @@ function set_edth!(;
      )
    nx, ny = size(edth)
    p = (spin+boost)
-
+   
+   angular_matrix_mult!(raised,level,@view Op.raise[:,:,Op.mmap[m_ang],Op.smap[spin]])
+   
    for j=1:ny
       for i=1:nx
          edth[i,j] = (
@@ -176,7 +220,8 @@ end
 
 function set_edth_prime!(;
       edth_prime::Array{ComplexF64,2},
-      spin::Int64, 
+      spin ::Int64, 
+      m_ang::Int64, 
       boost::Int64, 
       level  ::Array{ComplexF64,2},
       DT     ::Array{ComplexF64,2}, 
@@ -185,6 +230,8 @@ function set_edth_prime!(;
      )
    nx, ny = size(edth)
    q = (-spin+boost)
+
+   angular_matrix_mult!(lowered,level,@view Op.lower[:,:,Op.mmap[m_ang],Op.smap[spin]]) 
 
    for j=1:ny
       for i=1:nx
@@ -202,8 +249,8 @@ end
 
 function set_thorn!(;
       thorn::Array{ComplexF64,2},
-      m_ang::Int64,
       spin ::Int64, 
+      m_ang::Int64,
       boost::Int64, 
       falloff::Int64, 
       level::Array{ComplexF64,2}, 
